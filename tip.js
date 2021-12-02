1b46df95baec01bc6fcc8a4d4617cd37ddb3d69c
vishalvk <vishal.vk0057@gmail.com>
2021-12-02 18:06:54 +0530

    tip commit

---
+++ tip.js
@@ -0,0 +1,35 @@
+function calculateTip() {
+  var billAmt = document.getElementById("billAmt").value;
+  var serviceQual = document.getElementById("serviceQual").value;
+  var numOfPeople = document.getElementById("peopleAmt").value;
+
+  // validate input
+  if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter value");
+    return;
+  }
+  //check to see if this is empty or less than or equal to 1
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  //calculate tip
+  var total = (billAmt * serviceQual) / numOfPeople;
+  //round to two decimal places
+  total = Math.round(total);
+  //Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+}
+
+//Hide the tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click to call a function
+document.getElementById("calculate").onclick = function () {
+  calculateTip();
+};
